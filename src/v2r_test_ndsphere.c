1b88049d5bdba5c3d15ae4b269b14e8cef75b0fb
Sebastien Brisard <sebastien.brisard@m4x.org>
2019-11-24 15:31:48 +0100

    [Disk.belongs, Sphere.belongs] Factor tests

--- src/v2r_test_ndsphere.c
+++ src/v2r_test_ndsphere.c
@@ -43,35 +43,7 @@ void test_sphere_new() {
   v2r_object_free(sphere);
 }
 
-void v2r_setup_test_disk_belongs() {
-  size_t const dim = 2;
-  double const c[] = {1.2, -3.4};
-  double const r = 7.8;
-  V2R_Object *disk = v2r_disk_new(c, r);
-
-  double *directions = v2r_test_generate_directions(dim);
-  double *n = directions;
-  double const r_in = 0.95 * r;
-  double const r_out = 1.05 * r;
-
-  char name[256];
-  for (size_t i = 0; i < v2r_test_get_num_directions(dim); i++, n += dim) {
-    double const p1[] = {c[0] + r_in * n[0], c[1] + r_in * n[1]};
-    double const p2[] = {c[0] + r_out * n[0], c[1] + r_out * n[1]};
-
-    sprintf(name, "/disk/belongs/in/%02d", (int)i);
-    g_test_add_data_func_full(name, v2r_test_belongs_data_new(disk, p1, true),
-                              v2r_test_belongs, v2r_test_belongs_data_free);
-
-    sprintf(name, "/disk/belongs/out/%02d", (int)i);
-    g_test_add_data_func_full(name, v2r_test_belongs_data_new(disk, p2, false),
-                              v2r_test_belongs, v2r_test_belongs_data_free);
-  }
-  free(directions);
-  v2r_object_free(disk);
-}
-
-void v2r_setup_test_sphere_belongs(V2R_Object const *ndsphere) {
+void v2r_setup_test_ndsphere_belongs(V2R_Object const *ndsphere) {
   const size_t dim = ndsphere->type->dim;
   double const *directions = v2r_test_generate_directions(dim);
   double const *n = directions;
@@ -87,12 +59,12 @@ void v2r_setup_test_sphere_belongs(V2R_Object const *ndsphere) {
       p_out[j] = ndsphere->center[j] + r_out * n[j];
     }
 
-    sprintf(name, "/sphere/belongs/in/%02d", (int)i);
+    sprintf(name, "/%s/belongs/in/%02d", ndsphere->type->name, (int)i);
     g_test_add_data_func_full(name,
                               v2r_test_belongs_data_new(ndsphere, p_in, true),
                               v2r_test_belongs, v2r_test_belongs_data_free);
 
-    sprintf(name, "/sphere/belongs/out/%02d", (int)i);
+    sprintf(name, "/%s/belongs/out/%02d", ndsphere->type->name, (int)i);
     g_test_add_data_func_full(name,
                               v2r_test_belongs_data_new(ndsphere, p_out, false),
                               v2r_test_belongs, v2r_test_belongs_data_free);
@@ -108,8 +80,8 @@ void v2r_setup_test_ndsphere() {
   double const r = 7.8;
   V2R_Object *disk = v2r_disk_new(c, r);
   V2R_Object *sphere = v2r_sphere_new(c, r);
-  v2r_setup_test_disk_belongs();
-  v2r_setup_test_sphere_belongs(sphere);
+  v2r_setup_test_ndsphere_belongs(disk);
+  v2r_setup_test_ndsphere_belongs(sphere);
   v2r_object_free(disk);
   v2r_object_free(sphere);
 }
