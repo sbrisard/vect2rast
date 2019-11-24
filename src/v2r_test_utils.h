32c5c84b96b4d762c38fe3a951e8db70787ea065
Sebastien Brisard <sebastien.brisard@m4x.org>
2019-11-24 06:50:13 +0100

    Test utils

---
+++ src/v2r_test_utils.h
@@ -0,0 +1,16 @@
+#ifndef __V2R_TEST_UTILS_H__
+#define __V2R_TEST_UTILS_H__
+#include "vect2rast.h"
+
+typedef struct V2R_TestBelongsData_ {
+  V2R_Object *object;
+  double *point;
+  bool belongs;
+} V2R_TestBelongsData;
+
+DllExport void *v2r_test_belongs_data_new(V2R_Object *object,
+                                          double const *point, bool belongs);
+DllExport void v2r_test_belongs_data_free(void *data_);
+DllExport void v2r_test_belongs(void const *data_);
+
+#endif
