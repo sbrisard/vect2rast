e5dcd11ec63b3bf0a30df3d68be99dab7ea2f3ea
Sébastien Brisard <sebastien.brisard@m4x.org>
2020-01-12 08:23:44 +0100

    Simplified tests

--- src/v2r_test_utils.c
+++ src/v2r_test_utils.c
@@ -3,30 +3,6 @@
 
 #include "v2r_test_utils.h"
 
-void *v2r_test_belongs_data_new(V2R_Object const *object, double const *point,
-                                bool belongs) {
-  V2R_TestBelongsData *data = malloc(sizeof(V2R_TestBelongsData));
-  data->object = v2r_object_copy(object, NULL);
-  const size_t size = object->type->dim * sizeof(double);
-  data->point = malloc(size);
-  memcpy(data->point, point, size);
-  data->belongs = belongs;
-  return data;
-}
-
-void v2r_test_belongs_data_free(void *data) {
-  V2R_TestBelongsData *data_ = data;
-  v2r_object_free(data_->object);
-  free(data_->point);
-  free(data);
-}
-
-void v2r_test_belongs(void const *data) {
-  V2R_TestBelongsData const *data_ = data;
-  g_assert_cmpuint(data_->object->type->belongs(data_->object, data_->point),
-                   ==, data_->belongs);
-}
-
 V2R_TestRasterData *v2r_test_raster_data_new(V2R_Object *object,
                                              double const *length,
                                              size_t const *size) {
--- src/v2r_test_utils.h
+++ src/v2r_test_utils.h
@@ -2,17 +2,6 @@
 #define __V2R_TEST_UTILS_H__
 #include "vect2rast.h"
 
-typedef struct V2R_TestBelongsData_ {
-  V2R_Object *object;
-  double *point;
-  bool belongs;
-} V2R_TestBelongsData;
-
-DllExport void *v2r_test_belongs_data_new(V2R_Object const *object,
-                                          double const *point, bool belongs);
-DllExport void v2r_test_belongs_data_free(void *data);
-DllExport void v2r_test_belongs(void const *data);
-
 typedef struct V2R_TestRasterData {
   V2R_Object *object;
   double *length;
