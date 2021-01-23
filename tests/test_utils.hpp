3eae688a5f21cbbf5598ee7a54479e7fe220ab3c
Sébastien Brisard <sbrisard@users.noreply.github.com>
2021-01-23 19:00:25 +0100

    Use initializer list

--- tests/test_utils.hpp
+++ tests/test_utils.hpp
@@ -10,35 +10,26 @@
 
 template <size_t DIM>
 std::vector<std::array<double, DIM>> generate_directions() {
-  std::vector<std::array<double, DIM>> directions{};
   if constexpr (DIM == 2) {
     size_t const num_directions = 10;
+    std::vector<std::array<double, DIM>> directions{};
     for (size_t i = 0; i < num_directions; i++) {
       const double theta = 2 * M_PI * i / (double)num_directions;
       directions.push_back({cos(theta), sin(theta)});
     }
+    return directions;
   } else if constexpr (DIM == 3) {
     const double phi = .5 * (1. + sqrt(5.));
     const double u = 1. / sqrt(1 + phi * phi);
     const double v = phi * u;
-    directions.push_back({0., -u, -v});
-    directions.push_back({0., -u, +v});
-    directions.push_back({0., +u, -v});
-    directions.push_back({0., +u, +v});
-    directions.push_back({-u, -v, 0.});
-    directions.push_back({-u, +v, 0.});
-    directions.push_back({+u, -v, 0.});
-    directions.push_back({+u, +v, 0.});
-    directions.push_back({-v, 0., -u});
-    directions.push_back({-v, 0., +u});
-    directions.push_back({+v, 0., -u});
-    directions.push_back({+v, 0., +u});
+    return {{0., -u, -v}, {0., -u, +v}, {0., +u, -v}, {0., +u, +v},
+            {-u, -v, 0.}, {-u, +v, 0.}, {+u, -v, 0.}, {+u, +v, 0.},
+            {-v, 0., -u}, {-v, 0., +u}, {+v, 0., -u}, {+v, 0., +u}};
   } else {
     // This should never occur, since DIM was already statically asserted
     // through get_num_directions
     return nullptr;
   }
-  return directions;
 }
 
 std::array<double, 3> v2r_cross(const std::array<double, 3>& v1,
