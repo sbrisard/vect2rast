bb607f61e0a561242fec27210777e1c1df394c6d
Sébastien Brisard <sbrisard@users.noreply.github.com>
2021-01-25 08:15:14 +0100

    Remove unused command

--- tests/test_utils.hpp
+++ tests/test_utils.hpp
@@ -7,10 +7,6 @@
 
 #include "vect2rast/vect2rast.hpp"
 
-// DllExport void v2r_test_raster(V2R_Object const *object, double const
-// *length,
-//                               size_t const *size);
-
 template <size_t DIM>
 std::vector<std::array<double, DIM>> generate_directions() {
   if constexpr (DIM == 2) {
